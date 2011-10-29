6a8472696000bdbaea303714ac2f3e65c7120999
magnecyl <daniel@ljudbyran.se>
2011-10-29 16:40:24 +0200

    added styleing, progressbar, vu meter, timer on partyplayer. created a sound object for the pluginselect sound. Moved shortcut.js to goodies.js. Removed old dirty jquery 1.5.1. Added playlist features to partyplayer, enebled debug mode (set to false in partyplayer.js to remove). Added page-player.css for styleing of player.

--- partyplayer.js
+++ partyplayer.js
@@ -1,35 +1,35 @@
 // Partyplayer
 
-// directory where SM2 .SWFs live... This is a temporary url
-soundManager.url = 'http://dl.dropbox.com/u/10854052/';
+soundManager.flashVersion = 9;
+soundManager.debugMode = true;
+soundManager.preferFlash = true; // for visualization effects
+soundManager.useHighPerformance = true; // keep flash on screen, boost performance
+soundManager.wmode = 'transparent'; // transparent SWF, if possible
+soundManager.useFastPolling = true; // increased JS callback frequency
+soundManager.url = 'http://dl.dropbox.com/u/10854052/';//SWF URL
+
+// custom page player configuration
+
+var PP_CONFIG = {
+  autoStart: true,      // begin playing first sound when page loads
+  playNext: true,        // stop after one sound, or play through list until end
+  useThrottling: false,  // try to rate-limit potentially-expensive calls (eg. dragging position around)</span>
+  usePeakData: true,     // [Flash 9 only] whether or not to show peak data (left/right channel values) - nor noticable on CPU
+  useWaveformData: false,// [Flash 9 only] show raw waveform data - WARNING: LIKELY VERY CPU-HEAVY
+  useEQData: false,      // [Flash 9 only] show EQ (frequency spectrum) data
+  useFavIcon: false,     // try to apply peakData to address bar (Firefox + Opera) - performance note: appears to make Firefox 3 do some temporary, heavy disk access/swapping/garbage collection at first(?) - may be too heavy on CPU
+  useMovieStar: true     // Flash 9.0r115+ only: Support for a subset of MPEG4 formats.
+}
 
 soundManager.onready(function(){
-	var mySound = soundManager.createSound({
-		
-		id: 'Partymusic',
-		url: 'http://dl.dropbox.com/u/10854052/Music/01_Lfobia_%28Subsider_Remix%29-pMn_INT.mp3',
-		//http://dl.dropbox.com/u/10854052/Music/02%20morgan%20geist%20-%20the%20shore.mp3
-		//http://dl.dropbox.com/u/10854052/Music/Colony.mp3
+	var pluginSelectSnd = soundManager.createSound({
+		id: 'pluginSound',
+		url: 'http://dl.dropbox.com/u/10854052/lobbySfx-pluginselect.mp3',
 		autoLoad: true,
   		autoPlay: false,
 		stream: true,
-		
-		onid3: function() {	
-			soundManager._writeDebug('sound '+this.sID+' ID3 data received');
-			var prop = null;
-			var data = '';
-			for (prop in this.id3) {
-				data += prop+': '+this.id3[prop]+',';
-				$('.track-title').html(data + prop);
-			}
-  		},
-  		
-		onload: function() {
-    		//
-  		},
-  		//volume: 50
 	});
-	mySound.play();
+	pluginSelectSnd.play();
 });
 // Optional: ontimeout() callback for handling start-up failure
 soundManager.ontimeout(function(){
