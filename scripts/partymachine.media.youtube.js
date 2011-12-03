d60799bd015518868a29f88f403265939d447b5d
Joakim Persson <jocke@mejje.com>
2011-12-03 21:36:01 +0100

    Näbben stylign

--- scripts/partymachine.media.youtube.js
+++ scripts/partymachine.media.youtube.js
@@ -35,8 +35,8 @@
 		iframe.id = 'youtube';
 		iframe.frameBorder = 0;
 		iframe.style.position = 'fixed';
-		iframe.style.right = 0;
-		iframe.style.top = 0;
+		iframe.style.marginLeft = '35%';
+		iframe.style.bottom = 0;
 		iframe.style.zIndex = '-100';
 		iframe.style.visibility = 'hidden';
 		iframe.width = '30%';
