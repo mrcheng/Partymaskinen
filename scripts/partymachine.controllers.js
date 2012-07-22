cd0b7d478a04f214dd45858937eb869c238795c9
Joakim Persson <jocke@mejje.com>
2012-07-22 23:32:26 +0200

    Simplified plugin participant selector

--- scripts/partymachine.controllers.js
+++ scripts/partymachine.controllers.js
@@ -137,137 +137,27 @@
 	
 (function (controllerSelector, controllers, $, undefined) {
 
-	var _participants = [];
+	var _highlightedParticipantIndex;
 
-	var _highlightedControllersToParticipants = { };
 	var _assignedParticipantsToControllers = { };
 
-	var _colors	 = [
-		'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
+	function constructOverlay (participant) {
 
-	var _controllerColors = [];
-
-	function createCSSClass(selector, style) 
-	{
-		
-		if (!document.styleSheets) {
-			return;
+		var overlay = $('#assignGameControllersOverlay');
+		if (overlay.length == 0)
+		{
+			overlay = $('<div id="assignGameControllersOverlay" style="position: fixed; top: 50%; left: 50%; margin-top: -200px; margin-left: -128px; z-index: 999999; background-color: black; color: white; font-family: sans-serif; padding: 32px; text-align: center;"></div>');
+			$('body').append(overlay);
 		}
 
-		if (document.getElementsByTagName("head").length == 0) {
-			return;
-		}
-
-		var stylesheet;
-		var mediaType;
-		if (document.styleSheets.length > 0) {
-			for (i = 0; i < document.styleSheets.length; i++) {
-				if (document.styleSheets[i].disabled) {
-					continue;
-				}
-				var media = document.styleSheets[i].media;
-				mediaType = typeof media;
-
-				if (mediaType == "string") {
-					if (media == "" || (media.indexOf("screen") != -1)) {
-						styleSheet = document.styleSheets[i];
-					}
-				} else if (mediaType == "object") {
-					if (media.mediaText == "" || (media.mediaText.indexOf("screen") != -1)) {
-						styleSheet = document.styleSheets[i];
-					}
-				}
-
-				if (typeof styleSheet != "undefined") {
-					break;
-				}
-			}
-		}
-
-		if (typeof styleSheet == "undefined") {
-			var styleSheetElement = document.createElement("style");
-			styleSheetElement.type = "text/css";
-
-			document.getElementsByTagName("head")[0].appendChild(styleSheetElement);
-
-			for (i = 0; i < document.styleSheets.length; i++) {
-				if (document.styleSheets[i].disabled) {
-					continue;
-				}
-				styleSheet = document.styleSheets[i];
-			}
-
-			var media = styleSheet.media;
-			mediaType = typeof media;
-		}
-
-		if (mediaType == "string") {
-			for (i = 0; i < styleSheet.rules.length; i++) {
-				if (styleSheet.rules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
-					styleSheet.rules[i].style.cssText = style;
-					return;
-				}
-			}
-
-			styleSheet.addRule(selector, style);
-		} else if (mediaType == "object") {
-			for (i = 0; i < styleSheet.cssRules.length; i++) {
-				if (styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
-					styleSheet.cssRules[i].style.cssText = style;
-					return;
-				}
-			}
-
-			styleSheet.insertRule(selector + "{" + style + "}", 0);
-		}
-	}
-	
-	function removeParticipantHighlight(controllerId, participant) {
-		
-		var cssClass = 'pm-js-participant-controller-selection-' + controllerId;
-		
-		var pContainer = $('#pm-js-participant-controller-selection-' + participant);
-
-		pContainer.removeClass(cssClass);
-
-	}
-	
-	function addParticipantHighlight(controllerId, participant) {
-		
-		var borderColor;
-		
-		var cssClass = 'pm-js-participant-controller-selection-' + controllerId;
-		
-		if (!_controllerColors[controllerId]) {
-			_controllerColors[controllerId] = borderColor = _colors.pop();
-			createCSSClass('.' + cssClass, 'border: 5px solid ' + borderColor + '; -moz-border-radius: 15px; -webkit-border-radius: 15px');
-		}
-		
-		var pContainer = $('#pm-js-participant-controller-selection-' + participant);
-		
-		pContainer.addClass(cssClass);
-		
-	}
-	
-	function constructOverlay (participants) {
-
-		if (participants.length === 0) {
-			return;
-		}
-	 	
-	   var overlay = $('<div id="assignGameControllersOverlay" class="assignGameControllersOverlay"></div>');
-
-	   for (var i = 0; i < participants.length; i++) {
-	   	var currentParticipant = participants[i];
-	   	// TODO: Varför har jag inte ID här?
-	   	var participant = $('<div id="pm-js-participant-controller-selection-'+ i +'"><div class="participant-image"><img src="' + currentParticipant.imageUrl + '" width="128" height="128"></img></div><div class="name-container"><div class="fittext1 participant-name">' + currentParticipant.name +'</div></div></div>');
-	   	
-		participant.data("participantId", currentParticipant.id);
-	   	
-	   	overlay.append(participant);
-	   }
-	  
-	 	$('body').append(overlay);
+		overlay.html(
+			'<div class="participant-image">' +
+				'<img src="' + participant.imageUrl + '" width="256" height="256"></img>' +
+			'</div>' +
+			'<div class="name-container">' +
+				'<div class="fittext1 participant-name" style="font-weight: bold;">' + participant.name + '</div>' +
+				'press a button on your controller!' +
+			'</div>');
 	};
 
 	controllerSelector.assignGameControllers = function(
@@ -276,100 +166,50 @@
 		participantController
 	) {
 
-		_highlightedControllersToParticipants = { };
 		_assignedParticipantsToControllers = { };
-		
-		var navigateParticipant = function(controllerId, mod) {
+		_highlightedParticipantIndex = 0;
 
-			var participantCount = participants.length;
-			
-			var oldparticipant = (_highlightedControllersToParticipants[controllerId] > -1 ) ? _highlightedControllersToParticipants[controllerId] : -1;
+		constructOverlay(participants[_highlightedParticipantIndex]);
 
-			var participant = oldparticipant + mod;
-			
-			if (participant > participantCount - 1) {
-				participant = 0;
-			}
-			else if (participant < 0) {
-				participant = participantCount - 1;
-			}
-			
-			while(_assignedParticipantsToControllers[participant]) {
-				console.log("while(_assignedParticipantsToControllers[participant]) {");
-				participant += mod;
-				if (participant > participantCount - 1) {
-				participant = 0;
-				}
-				else if (participant < 0) {
-					participant = participantCount - 1;
-				}
-			}
-			
-			_highlightedControllersToParticipants[controllerId] = participant;
+		var buttonsPressed = function(buttonA, buttonB, buttonC, buttonD, controllerId)
+		{
+			_assignedParticipantsToControllers[_highlightedParticipantIndex] = controllerId;
 
-			if (oldparticipant != -1)
-				removeParticipantHighlight(controllerId, oldparticipant);
-				
-			addParticipantHighlight(controllerId, participant);
-			
-		};
-	
-		var gamepadPressed = function (left, up, right, down, controllerId) {
-			if (left) {
-				navigateParticipant(controllerId, -1);
-			} else if (right) {
-				navigateParticipant(controllerId, 1);
-			}
-		};
+			var highlightedParticipant = participants[_highlightedParticipantIndex];
+            controllers.mapController(
+				highlightedParticipant.gameController.gamepadPressed,
+				highlightedParticipant.gameController.gamepadReleased,
+				highlightedParticipant.gameController.buttonsPressed,
+				highlightedParticipant.gameController.buttonsReleased,
+				highlightedParticipant.gameController.joystick,
+				controllerId);
 
-		var buttonsPressed = function (buttonA, buttonB, buttonC, buttonD, controllerId) {
-		    var participant = _highlightedControllersToParticipants[controllerId];
-		   
-		    if (typeof participant === "undefined") {
-		        return;
-		    }
-		   
-		    if (typeof _assignedParticipantsToControllers[controllerId] !== "undefined") {
-				return;
-			}
+			_highlightedParticipantIndex++;
 
-			_assignedParticipantsToControllers[participant] = controllerId;
-
-            var p = participants[participant];
-
-            controllers.mapController(p.gameController.gamepadPressed, p.gameController.gamepadReleased, p.gameController.buttonsPressed, p.gameController.buttonsReleased, p.gameController.joystick, controllerId); 
-                      
-			if (Object.keys(_assignedParticipantsToControllers).length === participants.length)
+			if (_highlightedParticipantIndex == participants.length)
 			{
-			    if (typeof gameControllersAssigned === "undefined") {
-    			    return;
-    		    }
-
                 $("#assignGameControllersOverlay").remove();
 
 				var mappedControllers = [];
-				
-				for(var mappedParticipant in _assignedParticipantsToControllers) {
+
+				for (var mappedParticipant in _assignedParticipantsToControllers)
 					mappedControllers.push(_assignedParticipantsToControllers[mappedParticipant]);
-				}
 
 				controllers.unmapControllersExcept(mappedControllers);
-				
-    		    gameControllersAssigned();
 
+			    if (typeof gameControllersAssigned !== "undefined")
+    				gameControllersAssigned();
 			}
-            
-
-            		    
+			else
+				constructOverlay(participants[_highlightedParticipantIndex]);
 		};
 
-		var buttonsReleased = function() {};
+		var gamepadPressed = function() {};
 		var gamepadReleased = function() {};
+		var buttonsReleased = function() {};
 		var joystick = function() {};
 
 		controllers.mapControllers(gamepadPressed, gamepadReleased, buttonsPressed, buttonsReleased, joystick);
-
-		constructOverlay(participants);
 
 	};
 
